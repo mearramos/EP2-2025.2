43870e4582bfd19626877be2e40a72347bd584de
mearramos <ladykeer@gmail.com>
2025-10-24 10:33:37 -0300

    Define posições + preenche frota

---
+++ funcoes.py
@@ -0,0 +1,23 @@
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    retorno = []
+    if orientacao == "vertical":
+        i = linha
+        while i < linha + tamanho:
+            posicao = [i, coluna]
+            retorno.append(posicao)
+            i += 1
+    elif orientacao == "horizontal":
+        j = coluna
+        while j < coluna + tamanho:
+            posicao = [linha, j]
+            retorno.append(posicao)
+            j += 1
+    return retorno
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes)
+    else:
+        frota[nome_navio] = [posicoes]
+    return frota
