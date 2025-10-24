8d7efb7fe3b1bcf29126411b5b3b1360e9052827
mearramos <ladykeer@gmail.com>
2025-10-24 10:41:44 -0300

    Faz jogada

--- funcoes.py
+++ funcoes.py
@@ -21,3 +21,10 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
     else:
         frota[nome_navio] = [posicoes]
     return frota
+
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    elif tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    return tabuleiro
